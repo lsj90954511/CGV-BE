5e04b30626e29b0e7c90cb2b1e9eabf702ac8501
chaeeun-Han <chaeeun.h911@gmail.com>
2025-07-20 19:59:36 +0900

    공통 코드 및 dependencies 추가

---
+++ src/main/java/com/example/cgv/common/ResponseMessage.java
@@ -0,0 +1,27 @@
+package com.example.cgv.common;
+
+public interface ResponseMessage {
+    // HTTP Status 200
+    String SUCCESS = "success";
+
+    // HTTP Status 400
+    String NOT_EXISTED_USER = "This user does not exist.";
+    String NOT_EXISTED_EMAIL = "This email does not exist.";
+    String DUPLICATE_EMAIL = "duplicate email";
+    String DUPLICATE_ID = "duplicate id";
+
+    // HTTP Status 401
+    String VALIDATION_FAILED = "validation failed";
+    String SIGN_IN_FAILED = "Login information mismatch.";
+    String CERTIFICATE_FAIL = "certification failed";
+    String AUTHORIZATION_FAIL = "Authorization Failed.";
+    String INVALID_GRANT = "The provided access grant is invalid, expired, or revoked";
+    String EXPIRED_TOKEN = "This token is expired";
+
+    // HTTP Status 403
+    String NO_PERMISSION = "Do not have permission.";
+
+    // HTTP Status 500
+    String MAIL_FAIL = "mail send failed";
+    String DATABASE_ERROR = "database error";
+}
